f6336469b20a106a3f69ac5005c5ba9927f9ab04
BrendaCoronel <brendacarolinacoronel9@gmail.com>
2023-09-21 01:23:20 -0300

    Funcionalidad:Profesor

--- calificaciones.py
+++ calificaciones.py
@@ -29,6 +29,58 @@ def mostrar_perfil(rol):
         print("Bienvenido, Profesor")
         # Aquí puedes implementar la lógica para cargar y modificar notas.
 
+# Función para registrar una calificación para un alumno
+def registrar_calificacion(usuarios, nombre_usuario):
+    alumno = input("Ingrese el nombre del alumno: ")
+    asignatura = input("Ingrese la asignatura: ")
+    calificacion = input("Ingrese la calificación: ")
+
+    for usuario in usuarios:
+        if usuario['nombre_usuario'] == nombre_usuario and usuario['rol'] == 'profesor':
+            if 'calificaciones' not in usuario:
+                usuario['calificaciones'] = []
+
+            usuario['calificaciones'].append({
+                'alumno': alumno,
+                'asignatura': asignatura,
+                'calificacion': calificacion
+            })
+
+            with open('usuarios.json', 'w') as file:
+                json.dump({'usuarios': usuarios}, file, indent=4)
+            print("Calificación registrada con éxito.")
+            return
+
+    print("No se encontró al profesor.")
+
+# Función para consultar y modificar calificaciones
+def consultar_y_modificar_calificaciones(usuarios, nombre_usuario):
+    alumno = input("Ingrese el nombre del alumno: ")
+    asignatura = input("Ingrese la asignatura: ")
+
+    for usuario in usuarios:
+        if usuario['nombre_usuario'] == nombre_usuario and usuario['rol'] == 'profesor':
+            if 'calificaciones' in usuario:
+                for calificacion in usuario['calificaciones']:
+                    if calificacion['alumno'] == alumno and calificacion['asignatura'] == asignatura:
+                        print(f"Calificación actual: {calificacion['calificacion']}")
+                        nueva_calificacion = input("Ingrese la nueva calificación (deje en blanco para mantenerla igual): ")
+                        if nueva_calificacion:
+                            calificacion['calificacion'] = nueva_calificacion
+
+                            with open('usuarios.json', 'w') as file:
+                                json.dump({'usuarios': usuarios}, file, indent=4)
+                            print("Calificación modificada con éxito.")
+                        else:
+                            print("La calificación no se modificó.")
+                        return
+
+            print("No se encontró la calificación.")
+            return
+
+    print("No se encontró al profesor.")
+
+
 # Programa principal
 def main():
     print("Sistema de Gestión de Calificaciones")
@@ -41,6 +93,28 @@ def main():
         mostrar_perfil(rol)
     else:
         print("Credenciales incorrectas. Inicie sesión nuevamente.")
+
+    while True:
+        print("\nMENU")
+        print("1. Registrar Calificación")
+        print("2. Consultar y Modificar Calificaciones")
+        print("3. Registrar Alumno")
+        print("4. Salir")
+        opcion = input("Selecciona una opción: ")
+
+        usuarios = cargar_datos()
+
+        if opcion == "1":
+            registrar_calificacion(usuarios, nombre_usuario)
+        elif opcion == "2":
+            consultar_y_modificar_calificaciones(usuarios, nombre_usuario)
+        #elif opcion == "3":
+        #    registrar_alumno(usuarios)
+        elif opcion == "4":
+            print("Saliendo del menú.")
+            break
+        else:
+            print("Opción inválida. Por favor, selecciona una opción válida.")
 
 if __name__ == "__main__":
     main()
